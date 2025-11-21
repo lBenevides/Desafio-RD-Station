6319ad89dd01dc06771b8ffe7eca0b3afef311b4
Lucas Benevides <lhenriquebenevides@gmail.com>
2025-11-20 23:05:04 -0300

    criado model CartProducts para associação dos models e migration

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,6 @@ class Cart < ApplicationRecord
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  has_many :cart_products
+  has_many :products, through: :cart_products
 end
---
+++ app/models/cart_products.rb
@@ -0,0 +1,3 @@
+class CartProducts < ApplicationRecord
+
+end
