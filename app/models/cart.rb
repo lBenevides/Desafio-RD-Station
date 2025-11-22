8e3f5926cb3da5a1190d82613a1dd6fcb11a67e4
Lucas Benevides <lhenriquebenevides@gmail.com>
2025-11-22 09:28:38 -0300

    adicionado enum status no Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,10 +8,11 @@ class Cart < ApplicationRecord
   before_save :calculate_total_price
   after_destroy :calculate_total_price
 
+  enum status: { active: 0, abandoned: 1 }
+
   def calculate_total_price
     self.total_price = cart_products.sum do |item|
       item.product.price * item.quantity
     end
   end
-
 end
