809c5c4bb22c4d89a8ecab934ca8ef8e197b26e1
Lucas Benevides <lhenriquebenevides@gmail.com>
2025-11-22 20:32:54 -0300

    removido comentarios de TODO

--- spec/routing/carts_routing_spec.rb
+++ spec/routing/carts_routing_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe CartsController, type: :routing do
     end
 
     it 'routes to #create' do
-      # pending "#TODO: Escreva um teste para validar a criação de um carrinho #{__FILE__}" 
       expect(post: '/cart').to route_to('carts#create')
     end
 
