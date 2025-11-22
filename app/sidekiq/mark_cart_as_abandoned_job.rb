2b7e3394ad8b76ba1e812245be82132ff0fe4997
Lucas Benevides <lhenriquebenevides@gmail.com>
2025-11-22 19:10:26 -0300

    criado cobertura de testes para job MarkCartAsAbandonedJob

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -5,6 +5,11 @@ class MarkCartAsAbandonedJob
   def perform
     carts = Cart.where(status: 'active')
 
+    # Devido ao metodo mark_as_abandoned, optei por fazer com each loop
+    #  mas acho que seja mais performatico utilizar algo feito
+    #  Cart.where(status: 'active').where('last_interacion_at < ?', 3.hours.ago )
+    #  carts.update_all(status: 'abandoned')
+    #  isto evitaria loops e faria uma operação unica
     carts.each do |cart|
       cart.mark_as_abandoned
     end
--- spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
+++ spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
@@ -1,3 +1,15 @@
 require 'rails_helper'
 RSpec.describe MarkCartAsAbandonedJob, type: :job do
+
+  it 'marks carts with abandoned status when last interaction is more than 3 hours' do
+    cart = create(:cart, last_interaction_at: 3.hours.ago) 
+
+    expect { MarkCartAsAbandonedJob.new.perform }.to change { cart.reload.status }.from('active').to('abandoned')
+  end
+
+  it 'do not marks carts with abandoned status when last interaction is less than 3 hours' do
+    cart = create(:cart, last_interaction_at: 2.hours.ago) 
+
+    expect { MarkCartAsAbandonedJob.new.perform }.to_not change { cart.reload.status }
+  end
 end
