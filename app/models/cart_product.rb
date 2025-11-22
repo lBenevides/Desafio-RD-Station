3e3fcda0470e459f93bfa0740649a415a8bec8d5
Lucas Benevides <lhenriquebenevides@gmail.com>
2025-11-22 00:37:45 -0300

    Adicionado validações no CartProduct

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,4 +1,6 @@
 class CartProduct < ApplicationRecord
+  validates :product_id, uniqueness: { scope: :cart_id, message: 'já existe neste carrinho' }
+  validates_numericality_of :quantity, greater_than_or_equal_to: 0
 
   belongs_to :cart
   belongs_to :product
